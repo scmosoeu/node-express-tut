140a4bce765cfeb397fc25754221a12362b929eb
Selebogo Mosoeu <scmosoeu@gmail.com>
2023-06-05 04:29:37 +0200

    added async-patterns

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-const http = require('http')
+const {readFile} = require('fs')
+const { result } = require('lodash')
 
-const server = http.createServer((req, res) => {
-    console.log('request event')
-    res.end('Hello World')
-})
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, 'utf-8', (err, data) => {
+      if(err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
 
-// Asynchronous
-server.listen(5000, () => {
-    console.log('Server listening on port: 5000.....')
-})
+// getText('./content/first.txt')
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err))
+
+const start = async() => {
+  try {
+    const first = await getText('./content/first.txt')
+    const second = await getText('./content/second.txt')
+    console.log(first, second)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
