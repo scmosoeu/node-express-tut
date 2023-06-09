5f44eb5fe5a8346fe00226dba1a69174b5d89392
Selebogo Mosoeu <scmosoeu@gmail.com>
2023-06-09 21:37:07 +0200

    added request-event

---
+++ 13-event-emitter.js
@@ -0,0 +1,13 @@
+const EventEmitter = require('events');
+
+const customEmitter = new EventEmitter()
+
+customEmitter.on('response', () => {
+  console.log(`data recieved `)
+})
+
+customEmitter.on('response', () => {
+  console.log(`some other logic here `)
+})
+
+customEmitter.emit('response')
