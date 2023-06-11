18004f8a3c99f5b38944fa6a04db78bd6aba583b
Selebogo Mosoeu <scmosoeu@gmail.com>
2023-06-12 00:44:16 +0200

    added all files

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,58 +1,22 @@
 const express = require('express');
 const app = express();
-const {products} = require('./data')
+let { people } = require('./data')
 
-app.get('/', (req, res) => {
-  res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
-})
+// static assets
+app.use(express.static('./methods-public'))
 
-app.get('/api/products', (req, res) => {
-  const newProducts = products.map((product) => {
-    const {id, name, image} = product;
-    return {id, name, image}
+// parse form data
+app.use(express.urlencoded({ extended: false}))
+
+app.get('/api/people', (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: people
   })
-  res.json(newProducts)
 })
 
-app.get('/api/products/:productID', (req, res) => {
-  // console.log(req)
-  // console.log(req.params)
-
-  const {productID} = req.params;
-
-  const singleProduct = products.find((product) => product.id === Number(productID))
-
-  if(!singleProduct) {
-    return res.status(404).send('Product Does Not Exist')
-  }
-  res.json(singleProduct) 
-})
-
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-  console.log(req.params)
-  res.send('hello world')
-})
-
-app.get('/api/v1/query', (req, res) => {
-  // console.log(req.query)
-  const { search, limit } = req.query
-  let sortedProducts = [...products]
-
-  if(search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search)
-    })
-  }
-
-  if(limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit))
-  }
-
-  if(sortedProducts.length < 1) {
-    // res.status(200).send('no products matched your search')
-    return res.status(200).json({success: true, data: []})
-  }
-  res.status(200).json(sortedProducts)
+app.post('/login', (req, res) => {
+  res.send('POST')
 })
 
 app.listen(5000, () => {
